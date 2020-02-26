58e2e4bb9a68593d15e43f087475efe415f906b2
Arun Bisht <arun.bisht@nagarro.com>
2020-02-26 20:58:21 +0530

    Header and Footer compontent created

--- js/main.js
+++ js/main.js
@@ -1,32 +1,44 @@
 $(document).ready(function() { 
+    loadLeftMenu();
+    loadHeader();
+    
+    function loadLeftMenu() {
+        return $('.left-menu-wrapper').load("template/left-menu.html", function() { 
+            $(this).trigger('create');
+        });
+    }
 
-    $('.header-dropdown-trigger').on('click', function(event) {
+    function loadHeader() {
+        return $('.header-container').load("template/header.html", function() { 
+            $(this).trigger('create');
+        });
+    }
+
+    $('body').on('click', '.header-dropdown-trigger', function(event) {
         event.preventDefault();
         event.stopPropagation();
         $(this).parent('.header-dropdown-wrapper').toggleClass('active');
     });
 
-    $('.header-dropdown-wrapper .dropdown-menu').on('click', function(event) {
+    $('body').on('click', '.header-dropdown-wrapper .dropdown-menu', function(event) {
         event.stopPropagation();
     });
 
-    $('body').on('click', function(event) {
+    $('body').on('click', function() {
         $('.header-dropdown-wrapper').removeClass('active');
     });
 
-    $('.left-menu-trigger').on('click', function(event) {
+    $('body').on('click', '.left-menu-trigger', function(event) {
         event.preventDefault();
         $('body').toggleClass('left-menu-fixed');
     });
 
-    $('.left-sidebar-menu-wrapper .has-children > a').on('click', function(event) {
+    $('body').on('click', '.left-sidebar-menu-wrapper .has-children > a', function(event) {
         event.preventDefault();
         $(this).siblings('.child-menu').slideToggle();
         $(this).parent().toggleClass('active');
     });
-
-    $('.left-sidebar-menu-wrapper .tooltip').tooltip();
-
+    
     $(document).ready(function(){
         $('select').formSelect();
     });
