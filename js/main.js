4f75867de587755efa43e95023da0695be360e54
Arun Bisht <arun.bisht@nagarro.com>
2020-03-05 17:03:18 +0530

    View Item Advance Search Implimented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() { 
+
+    $('.tooltip').tooltip();
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
+
     loadLeftMenu();
     // loadHeader();
     
@@ -41,11 +46,72 @@ $(document).ready(function() {
         event.stopPropagation();
     });
 
-    $('.tooltip').tooltip();
+    $('body').on('click', '.modal-button-add-more', function(event) {
+        event.preventDefault();
+        var modalFormItem = $('.modal-form-item'), itemLength = modalFormItem.length, nextItemNum = (itemLength+1);
 
-    $('body').on('click', '.collapsible-data-table .collapsible-header .header-action-wrapper .input-field', function(event) {
-        // event.preventDefault();
-        // event.stopPropagation();
+        $(modalFormItem[itemLength - 1]).after('<div class="modal-form-item modal-form-item-'+nextItemNum+'" data-index="'+nextItemNum+'"><div class="row align-items-start justify-content-center"><div class="col-md-10 modal-form-input-wrapper"><div class="row align-items-start justify-content-center"></div></div><div class="col-md-2 modal-form-input-action-wrapper"><div class="button-wrapper"><a href="javascript:;" class="button-primary button-stroked tooltip modal-button-clear-row" data-tooltip="Clear Field"><i class="fas fa-times-circle"></i></a><a href="javascript:;" class="button-primary button-stroked tooltip modal-button-remove-row" data-tooltip="Remove Field"><i class="fas fa-minus-circle"></i></a></div></div></div></div>');
+
+        $('.modal-form-item-'+nextItemNum+' .modal-form-input-wrapper > .row').append('<div class="col-sm-4"> <div class="cell-wrapper"> <div class="input-field"> <select id="modal-attribute-list-'+nextItemNum+'" class="modal-attribute-list"> <option value="" disabled selected>Choose Attribute List</option> <option value="1">Option 1</option> <option value="2">Option 2</option> <option value="3">Option 3</option> </select> <label for="modal-attribute-list-'+nextItemNum+'">Attribute List</label> </div> </div> </div>');
+
+        $('.modal-form-item-'+nextItemNum+' .modal-form-input-wrapper > .row').append('<div class="col-sm-4"> <div class="cell-wrapper"> <div class="input-field"> <select id="modal-operator-'+nextItemNum+'" class="modal-operator"> <option value="" disabled selected>Choose Operator</option> <option value="contains">CONTAINS</option> <option value="equals">EQUALS</option> <option value="starts">STARTS</option> </select> <label for="modal-operator-'+nextItemNum+'">Operator</label> </div> </div> </div>');
+
+        $('.modal-form-item-'+nextItemNum+' .modal-form-input-wrapper > .row').append('<div class="col-sm-4"> <div class="cell-wrapper"> <div class="input-field"> <input placeholder="Enter default value" id="modal-value-'+nextItemNum+'" type="text" class="modal-value"> <label for="modal-value-'+nextItemNum+'" class="active">Value</label> </div> </div> </div>');
+
+        $('.modal-form-item-'+nextItemNum+' select').formSelect();
+        $('.modal-form-item-'+nextItemNum+' .tooltip').tooltip();
+
     });
+
+    $('body').on('click', '.modal-button-clear-row', function(event) { 
+        event.preventDefault();
+        $(this).closest('.modal-form-item').find('.modal-form-input-wrapper').find('.modal-attribute-list').val('');
+        $(this).closest('.modal-form-item').find('.modal-form-input-wrapper').find('.modal-operator').val('');
+        $(this).closest('.modal-form-item').find('.modal-form-input-wrapper').find('.modal-value').val('');
+
+        $(this).closest('.modal-form-item').find('.modal-form-input-wrapper').find('select').formSelect();
+    });
+
+    $('body').on('click', '.modal-button-remove-row', function(event) { 
+        event.preventDefault();
+        $(this).closest('.modal-form-item').remove();
+    });
+
+    $('body').on('click', '.removed-saved-search-modal', function() { 
+        var removedSavedSearchName = '',
+            removedSavedSearchIndex = '';
+
+        removedSavedSearchName = $(this).attr('data-name').trim();
+        removedSavedSearchIndex = $(this).attr('data-item').trim();
+
+        $('#deleteSavedSearchName').text(removedSavedSearchName);
+        $('.removed-saved-search').attr('data-delete-index', removedSavedSearchIndex);
+    });
+
+    $('body').on('click', '.removed-saved-search', function(event) { 
+        event.preventDefault();
+        var deleteIndex = $(this).attr('data-delete-index');
+        $('.removed-saved-search-modal[data-item="'+ deleteIndex +'"]').parent('.header-saved-search-item').remove();
+
+        var saveSearchLength = $('.removed-saved-search-modal').closest('.dropdown-content').children('li').length;
+        console.log(saveSearchLength);
+        if(saveSearchLength === 0) {
+            $('.save-search-trigger').parent('.button-primary').removeClass('button-with-dropdown');
+            $('.save-search-trigger').hide();
+        } else {
+            if(!$('.save-search-trigger').parent('.button-primary').hasClass('button-with-dropdown')) {
+                $('.save-search-trigger').parent('.button-primary').addClass('button-with-dropdown');
+            }
+            $('.save-search-trigger').show();
+        }
+    });
+
+    $('body').on('click', '.input-field-remove-trigger', function() {
+        $(this).closest('.header-adv-search-input-input-item').remove();
+     });
+
+    $('.tooltip').tooltip();
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
 
 });
