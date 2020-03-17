346726301df05878304be2f8769154fddd5fc42e
Arun Bisht <arun.bisht@nagarro.com>
2020-03-17 15:54:59 +0530

    Group management page created

---
+++ js/groups-management.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    var groupData;
+    $.get("data/groups-management.json", function(data, status){
+        groupData = data;
+        $(data).each(function(i) {
+            $('.groups-name-list').append('<li><a href="javascript:;"><i class="fas fa-user-friends"></i> '+ this.groups +'</a></li>');
+
+            if(i === 0) {
+                $('.groups-name-list li').addClass('active');
+                activeGpData(this);
+            }
+        });
+    });
+
+    
+    $('body').on('click', '.groups-name-list a', function(event) {
+        event.preventDefault();
+        var gpName = $(this).text().trim();
+        var gpData = {};
+        $(groupData).each(function() {
+            if(this.groups === gpName) {
+                gpData = this;
+            }
+        });
+        activeGpData(gpData);
+        $(this).parent('li').siblings().removeClass('active');
+        $(this).parent('li').addClass('active');
+
+    });
+
+    function activeGpData(gpData) {
+        $('.groups-description-wrapper p').text(gpData.description);
+
+        if(gpData.view_item === true) {
+            $('#group_management_view_item').prop("checked", true);
+        } else {
+            $('#group_management_view_item').prop("checked", false);
+        }
+
+        if(gpData.add_item === true) {
+            $('#group_management_add_item').prop("checked", true);
+        } else {
+            $('#group_management_add_item').prop("checked", false);
+        }
+
+        if(gpData.update_item === true) {
+            $('#group_management_update_item').prop("checked", true);
+        } else {
+            $('#group_management_update_item').prop("checked", false);
+        }
+
+        if(gpData.view_promo === true) {
+            $('#group_management_view_promo').prop("checked", true);
+        } else {
+            $('#group_management_view_promo').prop("checked", false);
+        }
+
+        if(gpData.add_promo === true) {
+            $('#group_management_add_promo').prop("checked", true);
+        } else {
+            $('#group_management_add_promo').prop("checked", false);
+        }
+    }
+});
